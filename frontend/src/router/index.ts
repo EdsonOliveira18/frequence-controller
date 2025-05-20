cb695269d7999450238c1a0b8b9607771a958549
Ivan-Duarte <igrod.dev@gmail.com>
2025-05-20 19:34:08 -0300

    feat: Adiciona tela inicial do ADM + modal com QRCode. Ajusta também exemplos para os .envs e o adiciona variavel publica para permitir o acesso dos celulares através do QRCode.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,9 +1,17 @@
 // src/router/index.ts
 import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
+
+
 import CadastroView from '@/views/CadastroView.vue'
+import InicioAdmView from '@/views/InicioAdmView.vue' 
 
 const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'InicioADM',
+    component: InicioAdmView
+  },
   {
     path: '/cadastro',
     name: 'Cadastro',
---
+++ frontend/src/views/InicioAdmView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <h1 class="text-3xl font-bold mb-8">Painel do Administrador</h1>
+
+    <button
+      @click="mostrarModal = true"
+      class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded"
+    >
+      + Novo Colaborador
+    </button>
+
+    <!-- Modal QRCode -->
+    <ModalQRCode v-if="mostrarModal" @fechar="mostrarModal = false" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import ModalQRCode from '@/components/ModalQRCode.vue'
+
+const mostrarModal = ref(false)
+</script>
